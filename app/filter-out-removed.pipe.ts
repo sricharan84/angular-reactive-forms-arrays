80b43a07c1dda1dae4b4ac5374c5b4a089c25175
sricharan84 <charan.eighty4@gmail.com>
2022-07-18 12:10:49 +0530

    2 files modified

--- app/filter-out-removed.pipe.ts
+++ app/filter-out-removed.pipe.ts
@@ -2,11 +2,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { RecordItem, RecordTypeEnum } from './student';
 @Pipe({
   name: 'filterOutRemoved',
-  pure: false
-  })
+  pure: false,
+})
 export class FilterOutRemovedPipe implements PipeTransform {
   transform(list: RecordItem[], args?: any): RecordItem[] {
-    console.log(list);
+    console.count('pipe');
     return list.filter((item) => item.recordType !== RecordTypeEnum.REMOVED);
   }
 }
